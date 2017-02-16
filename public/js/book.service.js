7b34d19dd426b852c8b8528c08b2684d3abc7f84
LalitKishor <lalitsinghbanna05@gmail.com>
2017-02-16 11:32:21 +0530

    add book

---
+++ public/js/book.service.js
@@ -0,0 +1,24 @@
+angular.module("theBukz")
+    .factory('Books', function($firebaseObject, $firebaseArray) {
+        var bookRef = firebase.database().ref('books');
+        var bookImageRef = firebase.storage().ref('bookImage');
+        var bookArray = $firebaseArray(bookRef);
+
+        var Book = {
+            getAllBook:function(){
+                return $firebaseObject(bookRef);
+            },
+            getBook: function(uid) {
+                return $firebaseObject(userRef.child(uid));
+            },
+            addBook:function(newValue){
+                bookRef.push().set(newValue);
+            },
+            uploadImage:function(uid,file){
+                bookImageRef.child(uid).put(file).then(function(snapshot){
+                    console.log(snapshot);
+                });
+            }
+        };
+        return Book;
+    });
